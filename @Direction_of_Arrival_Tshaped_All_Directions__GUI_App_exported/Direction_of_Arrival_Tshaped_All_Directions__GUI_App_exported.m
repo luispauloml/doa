91f1045a70b1236e05a002abcef0c6b2f5ca469e
Luis Paulo M. Lima <luispauloml@gmail.com>
2022-09-27 10:58:22 +0900

    Move get_source_position to classdef

--- @Direction_of_Arrival_Tshaped_All_Directions__GUI_App_exported/Direction_of_Arrival_Tshaped_All_Directions__GUI_App_exported.m
+++ @Direction_of_Arrival_Tshaped_All_Directions__GUI_App_exported/Direction_of_Arrival_Tshaped_All_Directions__GUI_App_exported.m
@@ -565,7 +565,41 @@ classdef Direction_of_Arrival_Tshaped_All_Directions__GUI_App_exported < matlab.
         end
 
         output = process_T_array(app, event, data)
-        output = get_source_position(app, varargin)
+
+        function [x, y] = get_source_position(app, varargin)
+            %% Get impact position based on the angles obtained from
+            %% T-arrays.
+
+            if ~isempty(varargin)
+                plot_flag = varargin{1} == 'plot';
+            end
+
+            m1 = tan(app.angles(1));
+            m2 = tan(app.angles(2) + pi);
+            x = app.distance / (m1 - m2);
+            y = m1 * x;
+            app.last_position = [x, y];
+            app.log(sprintf('position=(%0.1f, %0.1f)', x, y));
+
+            if plot_flag
+                fig = figure();
+                set(fig, 'Name', 'Source localization');
+                plot([0, x, 0], [0, y, app.distance], 'k--');
+                hold on;
+                plot([0, 0], [0, app.distance], 'bo');
+                plot(x, y, 'r*');
+                axis equal;
+                y_lims = [min(0, y), max(app.distance, y)]
+                x_lim = max([app.distance, diff(y_lims), 2*x]);
+                xlim([-x_lim/2, +x_lim/2])
+                ylim(y_lims)
+                grid on;
+                legend('Direction', 'Sensors', 'Source');
+                xlabel('x');
+                ylabel('y');
+                title(sprintf('Position: x=%0.1f, y=%0.1f', x, y));
+            end
+        end
     end
 
     % Assorted static methods
