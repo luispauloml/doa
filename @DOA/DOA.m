8b6a131ede915b545c2541563b41176f7628972a
Luis Paulo M. Lima <luispauloml@gmail.com>
2022-09-27 20:49:16 +0900

    Add quiet flag

--- @DOA/DOA.m
+++ @DOA/DOA.m
@@ -1,6 +1,24 @@
 classdef DOA
+    properties (Access = public)
+        quiet               logical % Flag to print log messages
+    end
     methods (Access = public)
         function self = DOA(direction_or_location, device_name, varargin)
+            self.quiet = false;
+
+            next_arg_is_value = false;
+            for i = 1 : length(varargin)
+                arg = varargin{i};
+                if ~next_arg_is_value
+                    switch arg
+                        case 'quiet'
+                            self.quiet = true;
+                            next_arg_is_value = false;
+                        otherwise
+                            error('invalid argument');
+                    end
+                end
+            end
         end
     end
 
@@ -14,6 +32,9 @@ classdef DOA
             %% msg : char
             %%     The message to be printed.
 
+            if self.quiet
+                return
+            end
             disp(msg);
         end
     end
