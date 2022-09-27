4fcee3925a7d44f7192bfd5c9419a7f7a77aba5d
Luis Paulo M. Lima <luispauloml@gmail.com>
2022-09-28 01:16:04 +0900

    Add get_source_position

--- @DOA/DOA.m
+++ @DOA/DOA.m
@@ -219,6 +219,23 @@ classdef DOA < handle
             end
             disp(msg);
         end
+
+        function [x, y] = get_source_position(self, a, b)
+            %% Calucate (x, y) position of the source.
+            %%
+            %% [x, y] = get_source_position(a, b)
+            %%
+            %% Parameters:
+            %% a : scalar
+            %%     The angle w.r.t the first T-array.
+            %% b : scalar
+            %%     The angle w.r.t the second T-array.
+
+            m1 = tan(a);
+            m2 = tan(b + pi);
+            x = self.distance / (m1 - m2);
+            y = m1 * x;
+        end
     end
 
     methods (Access = private, Static)
