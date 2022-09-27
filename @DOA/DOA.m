5a517a9bf064598746f52207777acd1427ca8957
Luis Paulo M. Lima <luispauloml@gmail.com>
2022-09-27 20:06:48 +0900

    Add log method

--- @DOA/DOA.m
+++ @DOA/DOA.m
@@ -3,4 +3,18 @@ classdef DOA
         function self = DOA(direction_or_location, device_name, varargin)
         end
     end
+
+    methods (Access = private)
+        function log(self, msg)
+            %% Print log message to console.
+            %%
+            %% [] = log(msg)
+            %%
+            %% Parameters:
+            %% msg : char
+            %%     The message to be printed.
+
+            disp(msg);
+        end
+    end
 end
